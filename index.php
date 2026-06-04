956d3a5dbf30c04eeaf6281a65f7ed417894e72c
Junhee Yoo <ihavnoid@yahoo.com>
2026-06-04 22:12:32 +0900

    feat: add viewport meta tag to prevent zoomed-out layout on mobile devices

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title>Text2MindMap</title>
 	<link rel="shortcut icon" type="image/png" href="favicon.png">
 
